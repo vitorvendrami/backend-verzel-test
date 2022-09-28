fd89da218afe443eb3c0553db37dc37af58b878f
vitor <vitor_vendrami@hotmail.com>
2022-09-28 12:30:52 -0300

    Enabling cors

--- code/app.py
+++ code/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
@@ -9,6 +10,7 @@ from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config.from_pyfile("config.py")
 
